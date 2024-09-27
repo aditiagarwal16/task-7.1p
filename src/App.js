ea31116aae5ca23348facf4db00803fa55620756
aditiagarwal <aditiagarwall1603@gmail.com>
2024-09-27 19:15:07 +1000

    deployed login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./components/Login";
+import { auth } from './firebase'; // Import Firebase auth
+import { signOut } from 'firebase/auth'; // Import signOut
 import SignUp from "./components/SignUp";
 import "./App.css";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setIsLoggedIn(!user);
+    });
+
+    return () => unsubscribe(); 
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('User logged out');
+      })
+      .catch((error) => {
+        console.error('Logout error:', error);
+      });
+  };
   return (
     <Router>
       <div className="navbar">
@@ -12,9 +35,9 @@ function App() {
             DEV@Deakin
           </Link>
           <input type="text" placeholder="Search..." className="search-input" />
-          <Link to="/login" className="nav-link">
-            Login
-          </Link>
+          {!isLoggedIn ? (
+          <Link to="/login"><button onClick={handleLogout}>Logout</button></Link>
+        ) :(<div></div>)}
         </div>
       </div>
       <Routes>
